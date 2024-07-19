05bba03d3e3ef862d8b84d49663c31ec499e91f5
Paula Henrsez <paulahenrsez@gmail.com>
2024-07-19 09:54:17 -0400

    Agregar archivos de proyecto.

---
+++ princSustitucionLiskov/Course.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace princSustitucionLiskov
+{
+    public abstract class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public abstract void Subscribe(Student std);
+        public abstract string GetCourseDetails();
+    }
+
+    public class OnlineCourse : Course
+    {
+        public override void Subscribe(Student std)
+        {
+            // write code to subscribe to an online course
+        }
+
+        public override string GetCourseDetails()
+        {
+            return $"Online Course: {Title} / ID: {CourseId}";
+        }
+    }
+
+    public class OnsiteCourse : Course
+    {
+        public override void Subscribe(Student std)
+        {
+            // write code to subscribe to an onsite course
+        }
+
+        public override string GetCourseDetails()
+        {
+            return $"Onsite Course: {Title} / ID: {CourseId}";
+        }
+    }
+
+    public class HybridCourse : Course
+    {
+        public override void Subscribe(Student std)
+        {
+            // write code to subscribe to an hybrid course
+        }
+
+        public override string GetCourseDetails()
+        {
+            return $"Hybrid Course: {Title} / ID: {CourseId}";
+        }
+    }
+
+}
---
+++ princSustitucionLiskov/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace princSustitucionLiskov
+{
+    public class Student
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Course onlineCourse = new OnlineCourse { CourseId = 976, Title = "ITLA1" };
+            Course onsiteCourse = new OnsiteCourse { CourseId = 809, Title = "ITLA2" };
+            Course hybridCourse = new HybridCourse { CourseId = 236, Title = "ITLA3" };
+
+            Student student = new Student { firstName = "Paula", lastName = "Henrsez" };
+
+            SubscribeAndDetails(onlineCourse, student);
+            SubscribeAndDetails(onsiteCourse, student);
+            SubscribeAndDetails(hybridCourse, student);
+        }
+
+        static void SubscribeAndDetails(Course course, Student student)
+        {
+            course.Subscribe(student);
+            Console.WriteLine(course.GetCourseDetails());
+        }
+    }
+}
